f95460d9436d62b92d1a99eb3a4edb90b4fd9935
Lawrence-tng <lxren460@gmail.com>
2021-09-28 20:55:44 -0700

    1st

--- A_StarPathfinding.py
+++ A_StarPathfinding.py
@@ -8,7 +8,7 @@ ROWS = 50
 win = pygame.display.set_mode((WIDTH, WIDTH))
 pygame.display.set_caption("A* Path Finding Algorithm")
 
-RED = (255, 0, 0)
+RED = (200, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 255, 0)
 YELLOW = (255, 255, 0)
